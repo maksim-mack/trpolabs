a112a1d97125ac3caa1169466e1a8531f6295ae7
Maksim <gerasimow.mack@gmail.com>
2020-04-15 19:09:16 +0300

    t4.1

--- maksim/MyLog.php
+++ maksim/MyLog.php
@@ -1,28 +1,21 @@
 <?php
-    namespace maksim;
-
-    /**
-     *
-     */
-    class Log extends \core\LogAbstract implements \core\LogInterface
-    {
-        public static function log($str)
-        {
+	namespace maksim;
+	/**
+	 * 
+	 */
+	class Log extends \core\LogAbstract implements \core\LogInterface
+	{
+		public static function log($str){
             self::Instance()->log[]=$str;
-        }
-        public static function write()
-        {
+		}
+   		public static function write(){
             self::Instance()->_write();
-        }
-        public function _write()
-        {
-            echo implode("\n", self::Instance()->log);
-
-            $d = new \DateTIme();
-            if (!is_dir("./log")) {
-                mkdir("./log");
+   		}
+   		public function _write(){
+            foreach (self::Instance()->log as $value)
+            {
+                echo $value;
+                echo "\n";
             }
-            $filename = $d -> format('d-m-Y\TH-i-s.u').".log";
-            file_put_contents("./log/".$filename, date(DATE_COOKIE)." | ".implode("\n", self::Instance()->log));
-        }
-    }
+   		}
+	}
